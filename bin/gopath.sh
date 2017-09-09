8d750dabce140c4b5e9f37663bf8c1af738db979
Knoppix User <carlosjulioperez@gmail.com>
2017-09-08 20:00:30 -0500

    Configuración para Knoppix 7.7.1

--- bin/gopath.sh
+++ bin/gopath.sh
@@ -1,2 +1,9 @@
 export GOROOT=/mnt-system/local/go
 export PATH=$PATH:$GOROOT/bin
+export GOPATH=/mnt-system/local/projects/gows
+
+#Obtener gotour
+sudo go get golang.org/x/tour/gotour
+
+#Ejecutar
+go tool tour
--- gitadd.sh
+++ gitadd.sh
@@ -1,7 +1,15 @@
-git add debs/
-git add esword/
-git add gitadd.sh
-git add programs/
-git add vim/
+git add vim/dotVim/bundle/neocomplete.vim
+git add vim/dotVim/bundle/nerdtree
+git add vim/dotVim/bundle/tagbar
+git add vim/dotVim/bundle/vim-go
+git add vim/dotVim/plugged/YCM-Generator
+git add vim/dotVim/plugged/gocode
+git add vim/dotVim/plugged/nerdtree
+git add vim/dotVim/plugged/ultisnips
+git add vim/dotVim/plugged/vim-easy-align
+git add vim/dotVim/plugged/vim-fireplace
+git add vim/dotVim/plugged/vim-github-dashboard
+git add vim/dotVim/plugged/vim-go
+git add vim/dotVim/plugged/vim-snippets
 
 git commit -m "Configuración para Knoppix 7.7.1"
