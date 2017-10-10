26d517f7cf8e8ec05e19f5ba81c6599f2b2aad12
Knoppix User <carlosjulioperez@gmail.com>
2017-10-10 17:44:16 +0200

    Ajustes en gnome-commander

--- configPrograms.sh
+++ configPrograms.sh
@@ -1,5 +1,6 @@
 #Configuración de accesos directos y programas
 cp -r /mnt-system/local/config/knoppix771/programs/geany ~/.config/
 cp /mnt-system/local/config/knoppix771/programs/panel ~/.config/lxpanel/LXDE/panels/
+cp /mnt-system/local/config/knoppix771/programs/gnome-commander ~/.gnome2/
 
 lxpanelctl restart
